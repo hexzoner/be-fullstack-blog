f8e2405a210a3f1b2fc844b34e8fb26d32bafb1f
hexzoner <hexzon@gmail.com>
2024-07-22 16:02:20 +0200

    added limit and offset query

--- index.js
+++ index.js
@@ -6,35 +6,47 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.get("/posts", async (req, res) => {
-  const client = new Client({
-    connectionString: process.env.PG_URI,
-  });
-  await client.connect();
-  const results = await client.query("SELECT * FROM posts;");
-  await client.end();
 
-  res.json(results.rows);
+app.get("/posts", async (req, res) => {
+  try {
+    const limit = req.query.limit || 10;
+    const offset = req.query.offset || 0;
+
+    const client = new Client({
+      connectionString: process.env.PG_URI,
+    });
+    await client.connect();
+    const results = await client.query("SELECT * FROM posts LIMIT $1 OFFSET $2;", [limit, offset]);
+    await client.end();
+
+    // console.log(results.rowCount);
+    res.json(results.rows);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 });
 
 app.post("/posts", async (req, res) => {
-  //   console.log(req.body);
-  const parsedBody = req.body;
+  try {
+    const parsedBody = req.body;
 
-  const client = new Client({
-    connectionString: process.env.PG_URI,
-  });
-  await client.connect();
-  const results = await client.query("INSERT INTO posts (author, title, cover, content, date) VALUES ($1, $2, $3, $4, $5) RETURNING *;", [
-    parsedBody.author,
-    parsedBody.title,
-    parsedBody.cover,
-    parsedBody.content,
-    parsedBody.date,
-  ]);
-  await client.end();
+    const client = new Client({
+      connectionString: process.env.PG_URI,
+    });
+    await client.connect();
+    const results = await client.query("INSERT INTO posts (author, title, cover, content, date) VALUES ($1, $2, $3, $4, $5) RETURNING *;", [
+      parsedBody.author,
+      parsedBody.title,
+      parsedBody.cover,
+      parsedBody.content,
+      parsedBody.date,
+    ]);
+    await client.end();
 
-  res.json(results.rows[0]);
+    res.json(results.rows[0]);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 });
 
 app.get("/posts/:id", (req, res) => res.json({ message: "GET a post by id" }));
