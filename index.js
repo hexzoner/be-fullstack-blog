2ed5c237659a8ca0345d4bbeee3481afb5f1f69a
hexzoner <hexzon@gmail.com>
2024-07-24 13:49:14 +0200

    added pagination

--- index.js
+++ index.js
@@ -11,17 +11,26 @@ app.use(express.json());
 
 app.get("/posts", async (req, res) => {
   try {
-    const limit = req.query.limit || 10;
-    const offset = req.query.offset || 0;
+    const limit = parseInt(req.query.limit) || 10;
+    // const offset = parseInt(req.query.offset) || 0;
+    const page = parseInt(req.query.page) || 1;
 
     const client = new Client({
       connectionString: process.env.PG_URI,
     });
     await client.connect();
-    const results = await client.query("SELECT * FROM posts ORDER BY date DESC LIMIT $1 OFFSET $2;", [limit, offset]);
+    const countResults = await client.query("SELECT COUNT(*) FROM posts;");
+    const totalResults = countResults.rows[0].count;
+    const totalPages = Math.ceil(totalResults / limit);
+    const queryOffset = (page - 1) * limit;
+    const results = await client.query("SELECT * FROM posts ORDER BY date DESC LIMIT $1 OFFSET $2;", [limit, queryOffset]);
     await client.end();
-    // console.log(results.rowCount);
-    res.json(results.rows);
+    // console.log("Total results:", totalResults);
+    // console.log("Total pages:", totalPages);
+    // console.log("Results per page:", limit);
+    // console.log("Current page", page);
+
+    res.json({ page, totalPages, totalResults: parseInt(totalResults), results: results.rows });
   } catch (error) {
     res.status(500).json(error.message);
   }
@@ -35,16 +44,13 @@ app.post("/posts", async (req, res) => {
       connectionString: process.env.PG_URI,
     });
     await client.connect();
-    const results = await client.query(
-      "INSERT INTO posts (author, title, cover, content, date) VALUES ($1, $2, $3, $4, $5) RETURNING *;",
-      [
-        parsedBody.author,
-        parsedBody.title,
-        parsedBody.cover,
-        parsedBody.content,
-        parsedBody.date,
-      ]
-    );
+    const results = await client.query("INSERT INTO posts (author, title, cover, content, date) VALUES ($1, $2, $3, $4, $5) RETURNING *;", [
+      parsedBody.author,
+      parsedBody.title,
+      parsedBody.cover,
+      parsedBody.content,
+      parsedBody.date,
+    ]);
     await client.end();
 
     res.json(results.rows[0]);
@@ -54,26 +60,21 @@ app.post("/posts", async (req, res) => {
 });
 
 app.get("/posts/:id", async (req, res) => {
-  
   try {
-  const id=req.params.id;
+    const id = req.params.id;
 
-  const client = new Client({
-    connectionString: process.env.PG_URI,
-  });
-  await client.connect();
-  const results = await client.query("SELECT * FROM posts WHERE id=$1;", [
-    id
-  ]); 
+    const client = new Client({
+      connectionString: process.env.PG_URI,
+    });
+    await client.connect();
+    const results = await client.query("SELECT * FROM posts WHERE id=$1;", [id]);
 
-  await client.end();
-  if (results.rowCount==0) 
-     res.status(404).json({ error: "User not found" })
-  else
-  res.json(results.rows[0]);
-} catch (error) {
-  res.status(500).json(error.message);
-}
+    await client.end();
+    if (results.rowCount == 0) res.status(404).json({ error: "User not found" });
+    else res.json(results.rows[0]);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 });
 
 app.put("/posts/:id", async (req, res) => {
@@ -85,10 +86,7 @@ app.put("/posts/:id", async (req, res) => {
       connectionString: process.env.PG_URI,
     });
     await client.connect();
-    const results = await client.query(
-      "UPDATE posts SET author = $1, title = $2, cover = $3, content = $4, date = $5 WHERE id = $6 RETURNING *;",
-      [author, title, cover, content, date, id]
-    );
+    const results = await client.query("UPDATE posts SET author = $1, title = $2, cover = $3, content = $4, date = $5 WHERE id = $6 RETURNING *;", [author, title, cover, content, date, id]);
     await client.end();
 
     res.json(results.rows[0]);
@@ -105,10 +103,7 @@ app.delete("/posts/:id", async (req, res) => {
       connectionString: process.env.PG_URI,
     });
     await client.connect();
-    const results = await client.query(
-      "DELETE FROM posts WHERE id = $1 RETURNING *;",
-      [id]
-    );
+    const results = await client.query("DELETE FROM posts WHERE id = $1 RETURNING *;", [id]);
     await client.end();
 
     res.json(results.rows[0]);
